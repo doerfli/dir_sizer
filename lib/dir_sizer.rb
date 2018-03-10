5d9d22252f3b6e85fb5477d514a5bdcc594fcd79
Marc Doerflinger <github@doerf.li>
2018-03-10 22:45:47 +0100

    implement navigation through directory hierarchy

--- lib/dir_sizer.rb
+++ lib/dir_sizer.rb
@@ -1,5 +1,6 @@
 require 'filesize'
 require 'terminal-table'
+require 'highline'
 
 class DirSizer
   def self.execute(dir)
@@ -9,25 +10,15 @@ class DirSizer
     end
     puts "Calculating size of directory #{dir}"
     size_hash = calculate_size_hash(dir)
-    sizes = calculate_size(size_hash)
+    contents = calculate_size(size_hash)
     puts 'done'
 
-    output = []
-    output << ['0',Filesize.from("#{sizes[:total]} B").pretty,dir]
-    i = 0
-    sizes[:dirs_by_size].each{ |t|
-      i = i+1
-      s = Filesize.from("#{t[1][:total]} B").pretty
-      output << [i,s,t[0]]
-    }
-
-    table = Terminal::Table.new :headings => ['Cmd', 'Dir', 'Size'], :rows => output
-    puts table
+    browse_contents(contents)
   end
 
   def self.calculate_size_hash(dir)
     print '.'
-    r = { :dirs => {}, :files => {}}
+    r = { :dirs => {}, :files => {}, :dir => dir}
     Dir.entries(dir).each { |e|
       next if ['.', '..'].include? e
       t = File.join(dir, e)
@@ -41,7 +32,7 @@ class DirSizer
   end
 
   def self.calculate_size(hash)
-    r = { :dirs => {}, :total => 0, :dirs_by_size => nil }
+    r = { :dirs => {}, :total => 0, :dirs_by_size => nil, :dir => hash[:dir] }
     size_files = hash[:files].values.inject { |sum, n| sum + n } || 0
     hash[:dirs].each { |d,h|
       size_d = calculate_size(h)
@@ -53,6 +44,38 @@ class DirSizer
     r[:total] = size_files + size_dirs
     r[:dirs_by_size] = r[:dirs].sort_by{|d,h| h[:total] }.reverse
     r
+  end
+
+  def self.browse_contents(content)
+    cli = HighLine.new
+    contents = content
+    back_stack = []
+
+    loop do
+      output = []
+      output << ['.',Filesize.from("#{contents[:total]} B").pretty,contents[:dir]]
+      i = 0
+      contents[:dirs_by_size].each{ |t|
+        s = Filesize.from("#{t[1][:total]} B").pretty
+        output << [i,s,t[0]]
+        i = i + 1
+      }
+      table = Terminal::Table.new :headings => ['Cmd', 'Dir', 'Size'], :rows => output
+      puts table
+
+      a = cli.ask("Number of directory? ('e' to exit)")
+
+      case a
+      when 'e'
+        break
+      when '..'
+        contents = back_stack.pop
+      else
+        back_stack.push contents
+        next_dir = contents[:dirs_by_size][a.to_i][0]
+        contents = contents[:dirs][next_dir]
+      end
+    end
   end
 
 end
